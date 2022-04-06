2bdcfa319878dd194c557b712e09916eb95e18d8
Olivier BOSSAVIT <olivier.bossavit@student.junia.com>
2022-04-06 21:48:57 +0200

    javadoc sa mère

--- enterGameNameHere/Floor.java
+++ enterGameNameHere/Floor.java
@@ -6,15 +6,27 @@ public class Floor {
     protected Point[][] map;
     protected ArrayList<Entity> entities;
 
+
+    /**
+     * Floor constructor, creates a 2D array of Points which represents the map
+     * @param size the size of the map
+     */
     public Floor(int size){
         this.map = new Point[size][size];
         initFloor(size);
     }
 
+    /**
+     * The default floor constructor, builds a 15*15 map as a 2D array of points
+     */
     public Floor(){
         this(15);
     }
 
+    /**
+     * The initialization function of the floor, which defines the 2D array which will represent the map
+     * @param size the size of the map (size*size, it is a square)
+     */
     private void initFloor(int size) {
         for(int i=0; i<size; i++)
             for(int j=0; j<size; j++){
