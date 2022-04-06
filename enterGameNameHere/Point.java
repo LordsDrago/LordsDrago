13ddedc7fa3c439fe2e9298b060c25c1189631e6
Olivier BOSSAVIT <olivier.bossavit@student.junia.com>
2022-04-06 19:14:54 +0200

    main method

--- enterGameNameHere/Point.java
+++ enterGameNameHere/Point.java
@@ -1,5 +1,27 @@
 package enterGameNameHere;
 
 public class Point {
-    
+    protected int x;
+    protected int y;
+    protected boolean isWall;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void toggleIsWall() {
+        this.isWall = this.isWall == false;
+    }
 }
