8f4680116318abb31195d45d33e0519fb574e7a9
Olivier BOSSAVIT <olivier.bossavit@student.junia.com>
2022-04-16 12:04:10 +0200

    Created basic game structure (merde les javadocs)

--- enterGameNameHere/Game.java
+++ enterGameNameHere/Game.java
@@ -1,5 +1,20 @@
 package enterGameNameHere;
 
+import enterGameNameHere.Terrain.Floor;
+
 public class Game {
-    
+    protected Floor[] gameMap;
+    protected int curFloor = 0;
+
+    public Game(){
+        this.gameMap = new Floor[] {new Floor(), new Floor(), new Floor()};
+    }
+
+    public void displayCurrentFloor() {
+        this.gameMap[this.curFloor].displayFloor();
+    }
+
+    public void advanceFloor() {
+        this.curFloor++;
+    }
 }
