89205f7e92bfeb3150680b33a41a0f8a151acdd2
Olivier BOSSAVIT <olivier.bossavit@student.junia.com>
2022-04-19 01:44:58 +0200

    jsp ce que g changé mais je commit

--- enterGameNameHere/userInterface.java
+++ enterGameNameHere/userInterface.java
@@ -14,4 +14,9 @@ public class userInterface {
         System.out.print(">>>");
         return scan.nextLine();
     }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
 }
